e3582c5d2ad83bbbe3b97ed61ff128a1631502e2
ze-fernando <zefernando.dev@gmail.com>
2025-01-31 22:35:43 -0300

    bug:consumer service

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.API.Infra;
+using RabbitMQ.API.Repository;
+using RabbitMQ.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddControllers();
 builder.Services.AddRabbitMQ();
+
+builder.Services.AddScoped<PublisherService>();
+builder.Services.AddScoped<ConsumerService>();
 
 var conf = new RabbitMqConfig();
 builder.Configuration.GetSection("RabbitMqConfig").Bind(conf);
 
-Console.WriteLine($"RABBITMQ {RabbitMqConfig.Host}");
-
+builder.Services.AddDbContext<TransactionRepository>( options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Connection")
+    )
+);
 var app = builder.Build();
 
+app.MapControllers();
 app.Run();
